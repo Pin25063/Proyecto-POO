793709a6dbb81fca02264982ca1895fa8a4e19b7
Pin25063 <pin25063@uvg.edu.gt>
2025-09-05 19:38:48 -0600

    Implementacion de metodos y to string Estudiante

--- Proyecto/Estudiante.java
+++ Proyecto/Estudiante.java
@@ -6,4 +6,21 @@ private ArrayList<Sesion> historialSesiones;
         super(idUsuario, nombre, correo, contrasena, rol); 
         this.historialSesiones = historialSesiones;
     };
+    
+    public void agendarSesion(Sesion sesion) {
+        historialSesiones.add(sesion);  // Agregar la sesión al historial
+    }
+
+    public ArrayList<Sesion> getHistorialSesiones() {
+        return historialSesiones;
+    }
+
+    public void setHistorialSesiones(ArrayList<Sesion> historialSesiones) {
+        this.historialSesiones = historialSesiones;
+    }
+    //Los getters en Usuario ya estan heredados a Estudiante
+    @Override
+    public String toString() {
+        return super.toString() + ", Historial de sesiones: " + historialSesiones.size(); //Verificar si se utilizara asi
+    }
 }
