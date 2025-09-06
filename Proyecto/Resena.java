2bfc2556ce07fa8c119f1cc7d55e23c3138672dc
Pin25063 <pin25063@uvg.edu.gt>
2025-09-05 20:32:58 -0600

    Construccion base de resena

--- Proyecto/Resena.java
+++ Proyecto/Resena.java
@@ -2,4 +2,15 @@ public class Resena {
     private String autor;
     private int calificacion;
     private String comentario;
+
+    public Resena(String autor, int calificacion, String comentario) {
+        this.autor = autor;
+        this.calificacion = calificacion;
+        this.comentario = comentario;
+    }
+
+    @Override
+    public String toString() {
+        return "Reseña de " + autor + " - Calificación: " + calificacion + " Comentario: " + comentario;
+    }
 }
