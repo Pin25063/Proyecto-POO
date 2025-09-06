4f256367774c6e9a329ed6564b37a980cc9d3f6c
Pin25063 <pin25063@uvg.edu.gt>
2025-09-05 19:53:00 -0600

    Herencia de tutor, menos Horario

--- Proyecto/Tutor.java
+++ Proyecto/Tutor.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 
-public class Tutor {
+public class Tutor extends Usuario {
     private ArrayList<String> materias;
     private ArrayList<Horario> disponibilidad; //Horario todavia no esta creado
     private ArrayList<Resena> resenas;
+    private final double tarifa;
+
+    // Constructor
+    public Tutor(int idUsuario, String nombre, String correo, String contrasena, Rol rol, ArrayList<String> materias, double tarifa) {
+        super(idUsuario, nombre, correo, contrasena, rol);
+        this.materias = materias;
+        this.tarifa = tarifa;
+    }
+
 }
