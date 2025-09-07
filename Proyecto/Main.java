9829621c89150a67c5043dac7444ebda445426b0
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2025-09-07 14:58:29 -0600

    Mauricio Corado – 07/09/2025 – Escena JavaFX y contenedor base de LoginVista

--- Proyecto/Main.java
+++ Proyecto/Main.java
@@ -1,5 +1,18 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override 
+    public void start(Stage stage) {
+        LoginVista root = new LoginVista();
+        Scene scene = new Scene(root, 420, 520);
+        stage.setTitle("Gestor de Tutorías - Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        
+        launch(args);
     }
 }
