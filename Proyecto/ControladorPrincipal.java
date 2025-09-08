da55f382d9321995e5a5f6a0dea0f0420574df3a
Sebastian García <estuardosebastian10@gmail.com>
2025-09-07 18:46:05 -0600

    Creación de la estructura inicial del ControladorPrincipal

--- Proyecto/ControladorPrincipal.java
+++ Proyecto/ControladorPrincipal.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControladorPrincipal {
     
+    // ATRIBUTOS
+    private GestorDeDatos gestorDeDatos;
+    private Usuario usuarioActual;
+    private LoginVista loginVista;
+    private ArrayList<Usuario> listaDeUsuarios;
+
+    // CONSTRUCTOR: para inicializar el controlador
+    public ControladorPrincipal(LoginVista loginVista) {
+        this.gestorDeDatos = new GestorDeDatos();
+        this.loginVista = loginVista;
+    }
 }
