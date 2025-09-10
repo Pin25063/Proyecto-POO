9b6bf25f288266a4b5f73c215494abe8ce642972
Sebastian García <estuardosebastian10@gmail.com>
2025-09-09 21:50:47 -0600

    Configuraciones y más modificaciones para que javafx funcione

--- src/main/java/Tutor.java
+++ src/main/java/Tutor.java
@@ -2,6 +2,6 @@ import java.util.ArrayList;
 
 public class Tutor {
     private ArrayList<String> materias;
-    private ArrayList<Horario> disponibilidad; //Horario todavia no esta creado
+    // private ArrayList<Horario> disponibilidad; //Horario todavia no esta creado
     private ArrayList<Resena> resenas;
 }
