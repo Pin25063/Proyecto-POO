6694cd725a1ff6ef518bf261f5d925ed2b5abbdb
Cris <crisjavi1307@gmail.com>
2025-09-11 17:11:09 -0600

    Se crea la base de la vista de BusquedaVista

--- src/main/java/BusquedaVista.java
+++ src/main/java/BusquedaVista.java
@@ -1,3 +1,29 @@
-public class BusquedaVista {
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.ArrayList;
+
+public class BusquedaVista extends VBox{
+    private final TextField campoMateria = new TextField();
+    private final TableView<Tutor> tablaResultados = new TableView<>();
+    private final Button btnBuscar = new Button("Buscar");
+
+    private final ArrayList<Tutor> tutores = new ArrayList<>();
+
+    public BusquedaVista() {
+        setPadding(new Insets(24));
+        setSpacing(16);
+        setAlignment(Pos.CENTER);
+        setFillWidth(true);
+
+        // --- COMMIT 1 ---
+        Label titulo = new Label("Buscar Tutores por Materia");
+        titulo.getStyleClass().add("titulo");
+    }
     
 }
