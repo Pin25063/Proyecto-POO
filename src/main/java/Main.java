dee15b8fafbe9c746932e35b6cd55e2532e1d3f1
Cris <crisjavi1307@gmail.com>
2025-09-11 16:47:33 -0600

    Se agregan atributos a la Clase Tutor para poder crear la clase Busqueda Vista

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.net.URL;
 
 public class Main extends Application {
     @Override 
--- src/main/java/Tutor.java
+++ src/main/java/Tutor.java
@@ -1,7 +1,50 @@
 import java.util.ArrayList;
 
-public class Tutor {
+public class Tutor extends Usuario {
     private ArrayList<String> materias;
-    // private ArrayList<Horario> disponibilidad; //Horario todavia no esta creado
+    //private ArrayList<Horario> disponibilidad; //Horario todavia no esta creado
     private ArrayList<Resena> resenas;
+    private double tarifa; //Quitado el final
+
+    // Constructor
+    public Tutor(int idUsuario, String nombre, String correo, String contrasena, Rol rol, ArrayList<String> materias, double tarifa) {
+        super(idUsuario, nombre, correo, contrasena, rol);
+        this.materias = materias;
+        this.tarifa = tarifa;
+    }
+
+    //Cambiar los estados de sesion
+    /*public void aceptarSolicitud(Sesion sesion) {
+        sesion.setEstado(EstadoSesion.COMPLETADA);
+    }*/
+
+    /*public void rechazarSolicitud(Sesion sesion) {
+        sesion.setEstado(EstadoSesion.CANCELADA);
+    }*/
+
+    public String getNombre() {
+    return super.getNombre();
+}
+
+    public String getCorreo() {
+    return super.getCorreo();
+}
+
+    public ArrayList<String> getMaterias() {
+    return materias;
+}
+
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Materias: " + materias + ", Tarifa: " + tarifa;
+    }
 }
