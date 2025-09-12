8b1784a37d09bf12aa54d65f81c1d12f66e8dd25
Brian <5.68bol@gmail.com>
2025-09-12 02:14:21 -0600

    feat: Implementacion de constructor principal de Horario con validacion

--- src/main/java/Horario.java
+++ src/main/java/Horario.java
@@ -1,3 +1,23 @@
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Horario {
+    private DayOfWeek dia;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+    private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm");
+
+    // constructor principal
+
+    public Horario (DayOfWeek dia, LocalTime horaInicio, LocalTime horaFin){
+        if (horaInicio.isAfter(horaFin)){
+            throw new IllegalArgumentException("La hora de inicio no puede ser posterior a la hora de fin.");
+        }
+        this.dia = dia;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+    }
 
 }
