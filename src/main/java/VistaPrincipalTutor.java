840158bbb0a848cda3cfc6ad690f632a29815d18
Cris <crisjavi1307@gmail.com>
2025-09-11 19:51:26 -0600

    Se crean los atributos para la clase VistaPrincipalTutor

--- src/main/java/VistaPrincipalTutor.java
+++ src/main/java/VistaPrincipalTutor.java
@@ -1,3 +1,11 @@
-public class VistaPrincipalTutor {
-    
+import java.lang.classfile.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
+public class VistaPrincipalTutor extends VBox{
+    private Tutor tutorActual;
+    private Label lblNombre, lblCorreo, lblTarifas;
+    private ListView<String> listaMaterias;
+    private Button btnEditarPerfil, btnVerSesiones, btnVerResenas;
 }
