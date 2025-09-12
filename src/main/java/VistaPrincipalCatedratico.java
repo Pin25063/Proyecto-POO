02b70d08796a8cc22af1d220735a1180a0760dc0
Cris <crisjavi1307@gmail.com>
2025-09-12 17:57:31 -0600

    Se agrega la vista basica del perfil catedratico con nombre y correo

--- src/main/java/VistaPrincipalCatedratico.java
+++ src/main/java/VistaPrincipalCatedratico.java
@@ -1,3 +1,25 @@
-public class VistaPrincipalCatedratico {
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class VistaPrincipalCatedratico extends VBox{
     
+    private Catedratico catedraticoActual;
+    private Label lblNombre, lblCorreo;
+
+    public VistaPrincipalCatedratico(Catedratico catedratico) {
+        this.catedraticoActual = catedratico;
+
+        Label titulo = new Label("Perfil de catedratico");
+        
+        lblNombre = new Label("Nombre: " + catedratico.getNombre());
+        lblCorreo = new Label("Correo: " + catedratico.getCorreo());
+
+        setSpacing(10);
+        setPadding(new Insets(20));
+
+        getChildren().addAll(titulo, lblNombre, lblCorreo);
+    }
+
+
 }
