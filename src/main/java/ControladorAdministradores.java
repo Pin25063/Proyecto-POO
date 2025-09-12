45e1842a87857529ed1ec699bd012610d9b9e1e3
Sebastian García <estuardosebastian10@gmail.com>
2025-09-12 17:22:23 -0600

    Controlador, inyeccion FXML y atributos de ControladorAdministradores

--- src/main/java/ControladorAdministradores.java
+++ src/main/java/ControladorAdministradores.java
@@ -1,3 +1,37 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+// Controlador para la vista del Catedrático y roles de administrador
 public class ControladorAdministradores {
+    // Se hace la inyección de componentes FXML
+
+    // @FXML enlaza las variables con los componentes del archivo FXML
+    @FXML
+    private ComboBox<Estudiante> estudianteComboBox;
     
+    @FXML
+    private ComboBox<Tutor> tutorComboBox;
+
+    @FXML 
+    private ComboBox<Curso> cursoComboBox;
+
+    @FXML
+    private Button asignarButton;
+
+    @FXML
+    private Label statusLabel;
+
+    // ATRIBUTOS
+    private GestorDeDatos gestorDatos;
+    private ArrayList<Usuario> listaDeUsuarios;
+    private ArrayList<Sesion> listaDeSesiones;
+    private ArrayList<Curso> listaDeCursos;
 }
