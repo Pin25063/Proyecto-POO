c96734434116f8fec51685d4121c03c9f0026f15
Pin25063 <pin25063@uvg.edu.gt>
2025-09-12 16:35:26 -0600

    Implementacion herencia en Catedratico

--- src/main/java/Catedratico.java
+++ src/main/java/Catedratico.java
@@ -1,5 +1,17 @@
 import java.util.ArrayList;
 
-public class Catedratico {
+public class Catedratico extends Usuario {
     private ArrayList<Curso> cursosACargo;
+
+    public Catedratico(int idUsuario, String nombre, String correo, String contrasena, Rol rol) {
+        super(idUsuario, nombre, correo, contrasena, rol);
+    }
+
+    public ArrayList<Curso> getCursos(){
+        return this.cursosACargo;
+    }
+
+    public void asignarTutoria(Estudiante estudiante, Tutor tutor, Curso curso){
+
+    }
 }
