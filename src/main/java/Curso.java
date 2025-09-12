e524c0daa5c9d398589e854c98f297734fbe9a63
Brian <5.68bol@gmail.com>
2025-09-11 23:34:27 -0600

    constructor principal de clase Curso

--- src/main/java/Curso.java
+++ src/main/java/Curso.java
@@ -1,4 +1,11 @@
 public class Curso {
     private String nombreCurso;
     private int codigoCurso;
+
+    //constructor
+
+    public Curso(int codigoCurso, String nombreCurso) {
+        this.codigoCurso = codigoCurso;
+        this.nombreCurso = nombreCurso;
+    }
 }
