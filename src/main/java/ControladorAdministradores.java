4f2143d90171ce61d30b145c64e3b0ddd7fa503f
Sebastian García <estuardosebastian10@gmail.com>
2025-09-12 17:38:55 -0600

    Creación de constructor y método initialize junto con filtros

--- src/main/java/ControladorAdministradores.java
+++ src/main/java/ControladorAdministradores.java
@@ -2,11 +2,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 // Controlador para la vista del Catedrático y roles de administrador
@@ -31,7 +33,52 @@ public class ControladorAdministradores {
 
     // ATRIBUTOS
     private GestorDeDatos gestorDatos;
-    private ArrayList<Usuario> listaDeUsuarios;
-    private ArrayList<Sesion> listaDeSesiones;
+    private List<Usuario> listaDeUsuarios;
+    private List<Sesion> listaDeSesiones;
     private ArrayList<Curso> listaDeCursos;
+
+    // CONSTRUCTOR 
+    public ControladorAdministradores(){
+        this.gestorDatos = new GestorDeDatos();
+    }
+
+    // Método que se manda a llamar automáticamente por JavaFX
+    @FXML
+    public void initialize(){
+        
+        // cargar datos del modelo
+        try {
+            this.listaDeUsuarios = gestorDatos.cargarUsuarios();
+            this.listaDeSesiones = gestorDatos.cargarSesiones();
+            
+        } catch (Exception e) {
+            // Alertar al usuario si existe un error
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error de carga");
+            alert.setHeaderText("No se pudieron cargar los datos");
+            alert.setContentText("Ocurrió un error al cargar la información: " + e.getMessage());
+            alert.showAndWait();
+        }
+
+        // Filtrar estudiantes y tutores
+        ObservableList<Estudiante> estudiantes = FXCollections.observableArrayList(
+            listaDeUsuarios.stream()
+                .filter(u -> u instanceof Estudiante)
+                .map(u -> (Estudiante) u)
+                .collect(Collectors.toList())
+        );
+
+        ObservableList<Tutor> tutores = FXCollections.observableArrayList(
+            listaDeUsuarios.stream()
+                .filter(u -> u instanceof Tutor)
+                .map(u -> (Tutor) u)
+                .collect(Collectors.toList())
+        );
+
+        // Mostrar datos filtrados en los ComboBox
+        estudianteComboBox.setItems(estudiantes);
+        tutorComboBox.setItems(tutores);
+        
+        
+    }
 }
