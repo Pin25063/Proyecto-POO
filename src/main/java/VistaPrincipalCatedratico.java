9a925e7929f2b70fc3db0f52812365078cb68937
Cris <crisjavi1307@gmail.com>
2025-09-12 18:26:08 -0600

    Se agregaron los botones para asignar las tutorias a los estudiantes y para ver las solicitudes

--- src/main/java/VistaPrincipalCatedratico.java
+++ src/main/java/VistaPrincipalCatedratico.java
@@ -1,6 +1,10 @@
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class VistaPrincipalCatedratico extends VBox{
@@ -12,6 +16,7 @@ public class VistaPrincipalCatedratico extends VBox{
         this.catedraticoActual = catedratico;
 
         Label titulo = new Label("Perfil de catedratico");
+        titulo.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
         
         lblNombre = new Label("Nombre: " + catedratico.getNombre());
         lblCorreo = new Label("Correo: " + catedratico.getCorreo());
@@ -33,7 +38,25 @@ public class VistaPrincipalCatedratico extends VBox{
         listaCursos.setPrefHeight(120);
 
         getChildren().addAll(lblCursos, listaCursos);
+
+        Button btnAsignarTutoria = new Button("Asignar tutoria");
+        Button btnVerSolicitudes = new Button("Ver solicitudes");
+
+        HBox contenedorBotones = new HBox(10, btnAsignarTutoria, btnVerSolicitudes);
+
+        contenedorBotones.setAlignment(Pos.CENTER);
+
+        getChildren().addAll(contenedorBotones);
+
+        btnAsignarTutoria.setOnAction(e -> mostrarInfo("Asignar Tutoria","Aqui se podran asignar tutorias a los estudiantes que lo requieran."));
+        btnVerSolicitudes.setOnAction(e -> mostrarInfo("Ver Solicitudes","Aqui se visualizaran las solicitudes de los estudiantes."));
     }
 
+    private void mostrarInfo(String titulo, String mensaje) {
+    Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+    alerta.setHeaderText(titulo);
+    alerta.setContentText(mensaje);
+    alerta.showAndWait();
+}
 
 }
