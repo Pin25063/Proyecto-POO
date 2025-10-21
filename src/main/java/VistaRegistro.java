69b309ef4a915f3284315759536511049e90b865
Pin25063 <pin25063@uvg.edu.gt>
2025-10-21 17:13:00 -0600

    mostrar materias (CheckBoxes) si el rol es TUTOR

--- src/main/java/VistaRegistro.java
+++ src/main/java/VistaRegistro.java
@@ -2,6 +2,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -18,6 +19,9 @@ public class VistaRegistro extends VBox {
     //Combox para seleccionar rol
     private final ComboBox<Rol> comboRol = new ComboBox<>();
 
+    //Vbox donde se encontraran las materias
+    private final VBox contenedorMaterias = new VBox(5);
+
     //Botones
     private final Button btnRegistrar = new Button("Registrar");
     private final Button btnCancelar = new Button("Cancelar");
@@ -25,6 +29,11 @@ public class VistaRegistro extends VBox {
     //Referencias al controlador y navegador
     private Runnable onCancel;
     private ControladorPrincipal controlador;
+
+    //Array de las materias disponibles hasta ahora
+    private static final String[] MATERIAS = {
+        "Matemática", "Física", "Química", "Programación", "Estadística"
+    };
 
     public VistaRegistro() {
         //Estilo del layout
@@ -38,15 +47,21 @@ public class VistaRegistro extends VBox {
         Label titulo = new Label("Registro de Usuario");
         titulo.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
 
-        //Placeholder visual de los campos
+        //Placeholder visual de los campos bases
         txtNombre.setPromptText("Nombre completo");
         txtCorreo.setPromptText("correo@uvg.edu.gt");
         txtContrasena.setPromptText("Contraseña");
 
+        //Selector de Rol
         comboRol.getItems().setAll(Rol.values());
         comboRol.setPromptText("Selecciona tu rol");
 
-        // Contenedor para los botones
+        //Selector de materias si el tipo de usuario aplica
+        contenedorMaterias.setPadding(new Insets(10));
+        contenedorMaterias.setAlignment(Pos.CENTER_LEFT);
+        contenedorMaterias.setVisible(false); // Oculto por defecto
+
+        // Contenedor para los botones de registro y cancelacion
         HBox botones = new HBox(10, btnRegistrar, btnCancelar);
         botones.setAlignment(Pos.CENTER);
         
@@ -57,11 +72,15 @@ public class VistaRegistro extends VBox {
             txtCorreo,
             txtContrasena,
             comboRol,
+            contenedorMaterias,
             botones
         );
 
         // Al presionar Cancelar, se regresa a login
         btnCancelar.setOnAction(e -> onCancel.run());
+
+        //Cuando el comboRol se active, actualizar las materias dependiendo del tipo de usuario
+        comboRol.setOnAction(e -> actualizarOpcionesMaterias());
 
         // Por ahora el botón Registrar solo muestra mensaje
         btnRegistrar.setOnAction(e -> {
@@ -85,5 +104,23 @@ public class VistaRegistro extends VBox {
         alerta.setHeaderText(header);
         alerta.setContentText(contenido);
         alerta.showAndWait();
+    }
+
+    //Metodo para actualizar las materias dependiendo del tipo de usuario que se este creando
+    private void actualizarOpcionesMaterias() {
+        contenedorMaterias.getChildren().clear(); // Limpia las materias anteriores al cambiar el rol
+
+        Rol rolSeleccionado = comboRol.getValue();
+        if (rolSeleccionado == Rol.TUTOR) {
+            Label lbl = new Label("Selecciona tus materias:");
+            contenedorMaterias.getChildren().add(lbl);
+            for (String materia : MATERIAS) {
+                CheckBox cb = new CheckBox(materia);
+                contenedorMaterias.getChildren().add(cb);
+            }
+            contenedorMaterias.setVisible(true);
+        } else {
+            contenedorMaterias.setVisible(false);
+        }
     }
 }
