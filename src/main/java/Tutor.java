1701a053b791014fd053a7f5f79cb98a3f0d1f0e
Sebastian García <estuardosebastian10@gmail.com>
2025-11-13 21:36:34 -0600

    Eliminación de clases que no se utilizaron e información irrelevante

--- src/main/java/Tutor.java
+++ src/main/java/Tutor.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class Tutor extends Usuario {
     private ArrayList<String> materias;
     //private ArrayList<Horario> disponibilidad; //Horario todavia no esta creado
-    private ArrayList<Resena> resenas;
     private double tarifa;
 
     // Constructor
